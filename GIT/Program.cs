b6535c7125f687cbc08ceb3d465534379ded125b
Jacob Rousseau <Jacob@LAPTOP-MHF1IAQD>
2017-02-01 10:27:52 -0800

    beau commentaire utile

--- GIT/Program.cs
+++ GIT/Program.cs
@@ -10,6 +10,8 @@ namespace GIT
     {
         static void Main(string[] args)
         {
+            // ceci est un commentaire
+            Console.WriteLine("BITCH BETTER HAVE MY MONEY!");
         }
     }
 }
